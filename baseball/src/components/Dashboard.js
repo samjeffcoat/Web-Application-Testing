d1997414ecbed0690525d50661f15bd343f73743
Sam <Jeffcoat>
2019-10-31 15:33:22 -0500

    Dashboard component built in for scoreboard Operator

--- baseball/src/components/Dashboard.js
+++ baseball/src/components/Dashboard.js
@@ -1,8 +1,20 @@
-import react from 'react';
+import React from 'react';
 
+class Dashboard extends React.Component{
+    render() {
+        const {addBalls, addStrikes, addHit, addFoul}= this.props;
+        return(
+            <div>
+                <h1> Scoreboard Operator</h1>
+                <button onClick={addBalls}>Add Ball</button>
+                <button onClick={addStrikes}>Add Strike</button>
+                <button onClick={addHit}>Add Hit</button>
+                <button onClick={addFoul}>Add Foul</button>
 
+            </div>
 
-export const function Dashboard = () => {
-
-
+        )
+    }
 }
+
+export default Dashboard;
